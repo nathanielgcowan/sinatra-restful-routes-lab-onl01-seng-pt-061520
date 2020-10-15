ea550cde45853a4c82bf6e680dc670ac9907b256
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-10-15 23:30:19 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -13,4 +13,12 @@
 
 ActiveRecord::Schema.define(version: 20201015232818) do
 
+  create_table "recipes", force: :cascade do |t|
+    t.string   "name"
+    t.string   "ingredients"
+    t.string   "cook_time"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
 end
